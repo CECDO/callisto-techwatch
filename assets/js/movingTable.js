0e08ce50ac6c865ed312627c41396006e03a762f
alexandregrue <alexandre.grue@hotmail.com>
2021-09-30 17:39:37 +0200

    remove consol.log on

--- assets/js/movingTable.js
+++ assets/js/movingTable.js
@@ -2,7 +2,6 @@ const tableOfContent = document.querySelector(".tableOfContents")
 
 
 window.addEventListener('scroll', () => {
-    console.log('scroll!')
     if(window.scrollY > 265) {
         tableOfContent.classList.add("scroll");
     } else {
