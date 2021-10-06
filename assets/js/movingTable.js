3c0d6b8b34b021dcb1c0558e198b0e90aed036ee
Alexandre Grué <alexandre.grue@hotmail.com>
2021-10-06 16:24:56 +0200

    format document

--- assets/js/movingTable.js
+++ assets/js/movingTable.js
@@ -1,20 +1,20 @@
-const tableOfContent = document.querySelector(".tableOfContents")
+const tableOfContent = document.querySelector('.tableOfContents');
 
 window.addEventListener('scroll', () => {
-    console.log('scroll')
-    let scrollTop = $(window).scrollTop();
-    let docHeight = $(document).height();
-    let winHeight = $(window).height();
-    let scrollPercent = (scrollTop) / (docHeight - winHeight);
-	let scrollPercentRounded = Math.round(scrollPercent*100);
-    if(scrollPercentRounded > 6 && scrollPercentRounded < 99) {
-        tableOfContent.classList.remove("scrolldown");
-        tableOfContent.classList.add("scroll");
-    } else if(scrollPercentRounded > 99){
-        tableOfContent.classList.remove("scroll");
-        tableOfContent.classList.add("scrolldown");
-    } else {
-        tableOfContent.classList.remove("scrolldown");
-        tableOfContent.classList.remove("scroll");
-    }  
-}) 
+	console.log('scroll');
+	let scrollTop = $(window).scrollTop();
+	let docHeight = $(document).height();
+	let winHeight = $(window).height();
+	let scrollPercent = scrollTop / (docHeight - winHeight);
+	let scrollPercentRounded = Math.round(scrollPercent * 100);
+	if (scrollPercentRounded > 6 && scrollPercentRounded < 99) {
+		tableOfContent.classList.remove('scrolldown');
+		tableOfContent.classList.add('scroll');
+	} else if (scrollPercentRounded > 99) {
+		tableOfContent.classList.remove('scroll');
+		tableOfContent.classList.add('scrolldown');
+	} else {
+		tableOfContent.classList.remove('scrolldown');
+		tableOfContent.classList.remove('scroll');
+	}
+});
